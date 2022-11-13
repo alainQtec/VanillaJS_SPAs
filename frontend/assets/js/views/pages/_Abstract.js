cd3782b99e00521fada02c3928ef04d0754eadfd
Alain Herve <alain.1337dev@outlook.com>
2022-11-13 11:09:14 +0200

    Update _Abstract.js

--- frontend/assets/js/views/pages/_Abstract.js
+++ frontend/assets/js/views/pages/_Abstract.js
@@ -3,5 +3,12 @@ class Pageabstact {
     this.title = `Learning | Html and CSS`;
     this.description = `Description`;
   }
+  render() {
+    if (process.browser) {
+      console.log(process.browser && window.location.pathname);
+    } else {
+      console.log("No browser window");
+    }
+  }
 }
 export default Pageabstact;
